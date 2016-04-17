e7cedeb0375e15df4c78fd6c03441ec692907fe9
flykule <castleliu40@gmail.com>
2016-04-17 09:56:36 +0800

    第二阶段â

---
+++ app/src/main/java/udcastle/coolweather/activity/ChooseAreaActivity.java
@@ -0,0 +1,7 @@
+package udcastle.coolweather.activity;
+
+/**
+ * Created by Administrator on 2016/4/16.
+ */
+public class ChooseAreaActivity {
+}
---
+++ app/src/main/java/udcastle/coolweather/util/HttpCallbackListener.java
@@ -0,0 +1,7 @@
+package udcastle.coolweather.util;
+
+/**
+ * Created by Administrator on 2016/4/16.
+ */
+public class HttpCallbackListener {
+}
